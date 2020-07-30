f216a5a2b1ef894998b32138259d9c636a905b09
gilang <lintang.gilang01@gmail.com>
2020-07-30 10:54:50 +0700

    update

--- Hari ke 7/Tugas Hari ke 7 Lintang Gilang Pratama.py
+++ Hari ke 7/Tugas Hari ke 7 Lintang Gilang Pratama.py
@@ -9,20 +9,35 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-def saya():
-    public_tweets = api.home_timeline()
-    for tweet in public_tweets:
-        print(tweet.text)
-        print("----------------------------------------")
-
 def user():
     nama = "jokowi"
-    jumlah = 10
+    jumlah = 200
     hasil = api.user_timeline(id=nama, count=jumlah)
     for tweet in hasil:
         print(tweet.text)
         print("----------------------------------------")
 
-user()
+
+twit = []
+def test():
+    nama = "jokowi"
+    jumlah = 200
+    hasil = api.user_timeline(id=nama, count=jumlah)
+    for tweet in hasil:
+        twit.append(tweet.text)
+    #print(twit)
+
+    print("---------------------------------")
+
+    keyword = ["covid", "virus"]
+    list = []
+    for i in twit:
+        for x in keyword:
+            if x in i.lower():
+                list.append(i)
+
+    print(list)
+    print(len(list))
+test()
 
 print("done")
